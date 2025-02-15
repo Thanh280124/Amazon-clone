d08bfce8d64c1f3cb1cfd281e50297ac4af7dbe0
Pham Thanh <thanhpham28124@gmail.com>
2025-02-15 20:01:00 +0200

    delete product from cart button

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -35,7 +35,8 @@ cart.forEach((cartItem) =>{
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" 
+                  data-product-id = "${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -91,3 +92,13 @@ cart.forEach((cartItem) =>{
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((links) =>{
+links.addEventListener('click', () =>{
+  const productId = links.dataset.productId;
+  removeCart(productId);
+  console.log(cart);
+  
+  
+})
+})
